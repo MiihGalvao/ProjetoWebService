100c24cb90530bef764c12ef1201cefba22ac7a2
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-10-03 21:42:34 -0300

    Configurações do banco - tabela categorias

--- Senac-bike/app/Models/Categoria.php
+++ Senac-bike/app/Models/Categoria.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+
+    //campos publicáveis(preenchiveis)
+    protected $fillable = ['nomedacategoria'];
+
+    //nome da chave primária
+    protected $primaryKey = 'pkcategoria';
+
+    //nome da tabela
+    protected $table = 'categorias';
+
+    //informa que está trabalhando com incremento
+    public $incrementing = true;
+
+    //não utiliza timestamps nos controles (created & updated)
+    public $timestamps = false;
 }
