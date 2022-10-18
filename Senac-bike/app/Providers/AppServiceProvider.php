0db8d732758bd30173d6e770d5e8f3faa5c8ab7b
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-10-17 21:52:56 -0300

    ultima atualização passport

--- Senac-bike/app/Providers/AppServiceProvider.php
+++ Senac-bike/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+   
 
     /**
      * Bootstrap any application services.
